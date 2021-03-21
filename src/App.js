0a7bb08ca9cbc37145a665ed19a808b97157cddb
Junior Chagas <jjuniorchagas15@gmail.com>
2021-03-21 12:25:50 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
+import Tmdb from './Tmdb'; 
+import MovieRow from './components/MovieRow'; 
+
 
 export default () => {
+
+  const [ movieList, setMovieList ] = useState([]); 
+
+  useEffect(() => {
+    const loadAll = async () => {
+
+      //pegando a lista total
+      let list = await Tmdb.getHomeList(); 
+      setMovieList(list)
+    }; 
+
+    loadAll(); 
+  }, []); 
+
   return(
-    <div>
-      Olá Mundo!
+    <div className='page'>
+      <section className='lists' >
+        {movieList.map((item,key)=>(
+          <MovieRow 
+            key={key}
+            title={item.title}
+            items={item.items}
+          />
+        ))}
+      </section>
     </div>
   ); 
 }; 
+
+
+/*
+header
+destaque
+as listas
+rodapé basicao
+*/
+
